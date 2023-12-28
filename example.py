475789a9d4452e893188839f39ea10c48ac319d3
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-28 16:44:16 +0300

    random number

--- example.py
+++ example.py
@@ -1,8 +1,15 @@
-from math import sqrt,ceil
+# from math import sqrt,ceil
 
-x = sqrt(9)
-print(x)
+# x = sqrt(9)
+# print(x)
 
-y = ceil(x)
-print(y)
+# y = ceil(x)
+# print(y)
+
+# Introduction to random module
+import random
+# We can generate a random number using the randit method
+random_number = random.randint(0,10)
+print(random_number)
+
 
