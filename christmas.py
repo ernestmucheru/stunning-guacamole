c0c5b364c07fd5cfd2852b80471177d1c543d990
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-28 15:09:06 +0300

    Steve's bmi calculator

--- christmas.py
+++ christmas.py
@@ -1,6 +1,15 @@
-weight = float(input("Enter your weight: "))
-height = float(input("Enter your height: "))
+#Ken's BMI CALC
+# weight = float(input("Enter your weight: "))
+# height = float(input("Enter your height: "))
 
-bmi = "Your BMI is" + " ", (weight/height ** 2)
+# bmi = "Your BMI is" + " ", (weight/height ** 2)
+
+# print(bmi)
+
+#Steve's BMI calc
+weight = float(input('Enter your weight: '))
+height = float(input('Enter your height: '))
+
+bmi = weight/(height ** 2)
 
 print(bmi)
