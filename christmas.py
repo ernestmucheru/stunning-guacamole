895d41c0c8f5877ac1f600b9f08f18145463c984
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-28 14:17:57 +0300

    Simple computation

--- christmas.py
+++ christmas.py
@@ -1,1 +1,12 @@
-print('Hello, world.'.upper() + ' ' +'     I am new to Python.     '.strip())
+a = 15
+b = 10
+c = 20
+d = 2
+
+balance = c - b
+profit = c - a
+total = a + b + c
+metres_squared = a ** 2
+
+print(balance, profit, total, metres_squared)
+print("Your balance is" + ' ',balance)
