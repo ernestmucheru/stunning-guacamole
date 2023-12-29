e54a628695df2007649cbaffe5ac0cacf49fe51e
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-29 14:08:11 +0300

    built a tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,5 +1,19 @@
 bill = float(input('Enter your bill: '))
-tip = 0.15 * bill
-total_bill = tip + bill
+level_of_satisfaction = int(input('\nEnter \n\n1 for Great Service \n2 for Good Service \n3 for Terrible Service \n\nYour input goes here: ' ))
 
-print(f"Your total cost is {total_bill}")
+if level_of_satisfaction == 1:
+    tip = 0.2 * bill
+    total_bill = tip + bill
+    print(f"Your total cost is {total_bill}")
+elif level_of_satisfaction == 2:
+    tip = 0.15 * bill
+    total_bill = tip + bill
+    print(f"Your total cost is {total_bill}")
+else:
+    print(f"Your total cost is {bill}")
+
+
+
+
+
+
