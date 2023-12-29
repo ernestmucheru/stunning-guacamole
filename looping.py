ecde818c0a523a285c14a9291b3d1011ffaf9138
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-29 15:45:05 +0300

    for loops

---
+++ looping.py
@@ -0,0 +1,27 @@
+# FOR LOOPS
+# numbers = [1,2,3,4,5]
+# names = ["Ernest", "Peter", "Kennedy", "Stephen"]
+
+# for number in numbers:
+#     print(number)
+
+# using range in loops
+# list_a = list(range(0,5))
+# print(list_a)
+
+# using range in loops example
+# for i in range(0,7):
+#     print(f"I would love {i} cookies")
+
+# using for loop example
+# numbers = [1,2,3,4,5]
+# for i in numbers:
+#     if i % 2 != 0:
+#         print(i)
+
+# WHILE LOOPS
+players = 11
+
+while players >= 5:
+    print(f"The remaining players are {players}")
+    players -= 1
