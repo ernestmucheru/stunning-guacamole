27598864d3ab0f16b10ef3d1f6f957bf2172535f
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-30 11:55:52 +0300

    Introduction to functions

--- formatted_strings.py
+++ formatted_strings.py
@@ -18,5 +18,4 @@ verb_four= input("Enter a verb: ")
 adjective_two = input("Enter an adjective: ")
 color = input("Enter a color: ")
 
-
 print(f"Most doctors agree that bicycle of {verb_one} is a/an {adjective_one} form of exercise. {verb_two} a bicycle enables you to develop your {part_of_body_1} muscles as well as {adverb_one} increase the rate of a {part_of_body_2} beat. More  around the world {verb_three} bicycles than drive {animal_name}. No matter what kind of {noun_one} you {verb_four}, always be sure to wear a/an {adjective_two} helmet. Make sure to have {color} reflectors too!")
---
+++ functions.py
@@ -0,0 +1,33 @@
+# def fun_a():
+#     print("I have been called")
+
+# fun_a()
+
+# Passing arguments
+# def fun_a(a,b):
+#     print(a + b)
+
+# fun_a(1,4)
+
+# keyword arguments
+# def fun_a(a = 1,b = 4):
+#     print(a + b)
+
+# fun_a(a = 6, b = 7)
+
+# keyword arguments
+# def fun_a(a = 1,b = 4):
+#     print(a + b)
+
+# fun_a()
+
+# Empty function
+# def fun_a():
+#     pass
+
+def fun_a(a,b):
+    return a + b
+
+sum = fun_a(4,5)
+
+print(sum + 2)
