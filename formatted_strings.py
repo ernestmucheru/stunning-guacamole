de4b9d55b7a76e4c89089b467d443f58839f6acb
ernestmucheru <ernestmucheru254@gmail.com>
2023-12-30 11:13:52 +0300

    string_formatting

---
+++ formatted_strings.py
@@ -0,0 +1,22 @@
+# print('Beyonce\'s lemonade stand')
+
+# print("Beyonce's lemonade stand")
+
+# print(r'Beyonce\'s lemonade stand')\
+
+# Mad libs program
+verb_one = input("Enter a verb: ")
+adjective_one = input("Enter an adjective: ")
+verb_two = input("Enter a verb: ")
+part_of_body_1 = input("Enter a part of a body: ")
+adverb_one = input("Enter an adverb: ")
+part_of_body_2 = input("Enter a part of a body: ")
+verb_three = input("Enter a verb: ")
+animal_name = input("Enter any animals: ")
+noun_one = input("Enter a noun: ")
+verb_four= input("Enter a verb: ")
+adjective_two = input("Enter an adjective: ")
+color = input("Enter a color: ")
+
+
+print(f"Most doctors agree that bicycle of {verb_one} is a/an {adjective_one} form of exercise. {verb_two} a bicycle enables you to develop your {part_of_body_1} muscles as well as {adverb_one} increase the rate of a {part_of_body_2} beat. More  around the world {verb_three} bicycles than drive {animal_name}. No matter what kind of {noun_one} you {verb_four}, always be sure to wear a/an {adjective_two} helmet. Make sure to have {color} reflectors too!")
